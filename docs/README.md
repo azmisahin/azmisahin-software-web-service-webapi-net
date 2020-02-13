13f8962b714d44e1f570c238567b587613022d67
Azmi ŞAHIN <azmisahin@outlook.com>
2020-02-13 10:08:56 +0300

    Application User Password Validator

--- docs/README.md
+++ docs/README.md
@@ -10,3 +10,16 @@ It uses the ASP.NET Web API to create an API that returns a list of web products
 [Document Web Site](https://azmisahin.github.io/azmisahin-software-web-service-webapi-net/)
 
 ![api-documentation](media/api-documentation.png)
+
+# Password Policy
+IdentityConfig.cs
+```
+manager.PasswordValidator = new PasswordValidator
+{
+    RequiredLength = 6,
+    RequireNonLetterOrDigit = false,
+    RequireDigit = false,
+    RequireLowercase = false,
+    RequireUppercase = false,
+};
+```
--- src/Todo/App_Start/IdentityConfig.cs
+++ src/Todo/App_Start/IdentityConfig.cs
@@ -7,15 +7,26 @@ using Todo.Models;
 
 namespace Todo
 {
-    // Bu uygulamada kullanılan uygulama kullanıcı yöneticisini yapılandırın. UserManager ASP.NET Identity'de tanımlanır ve uygulama tarafından kullanılır.
-
+    /// <summary>
+    /// Bu uygulamada kullanılan uygulama kullanıcı yöneticisini yapılandırın. UserManager ASP.NET Identity'de tanımlanır ve uygulama tarafından kullanılır.
+    /// </summary>
     public class ApplicationUserManager : UserManager<ApplicationUser>
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="store"></param>
         public ApplicationUserManager(IUserStore<ApplicationUser> store)
             : base(store)
         {
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="options"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
         public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context)
         {
             var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<ApplicationDbContext>()));
@@ -29,10 +40,10 @@ namespace Todo
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
+                RequireLowercase = false,
+                RequireUppercase = false,
             };
             var dataProtectionProvider = options.DataProtectionProvider;
             if (dataProtectionProvider != null)
