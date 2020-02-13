7dd3abda4299adbeb6b14ab3c5d4f3bc10257ed0
Azmi ŞAHIN <azmisahin@outlook.com>
2020-02-13 10:48:57 +0300

    Services

--- Todo/App_Start/IdentityConfig.cs
+++ Todo/App_Start/IdentityConfig.cs
@@ -14,8 +14,16 @@ using Todo.Models;
 
 namespace Todo
 {
+    /// <summary>
+    /// Email Service
+    /// </summary>
     public class EmailService : IIdentityMessageService
     {
+        /// <summary>
+        /// Send Email
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
         public Task SendAsync(IdentityMessage message)
         {
             // E-posta göndermek için e-posta hizmetinizi buraya bağlayın.
@@ -23,8 +31,16 @@ namespace Todo
         }
     }
 
+    /// <summary>
+    /// Sms Service
+    /// </summary>
     public class SmsService : IIdentityMessageService
     {
+        /// <summary>
+        /// Send Sms
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
         public Task SendAsync(IdentityMessage message)
         {
             // Metin iletisi göndermek için SMS hizmetinizi buraya bağlayın.
@@ -32,14 +48,26 @@ namespace Todo
         }
     }
 
-    // Bu uygulamada kullanılan uygulama kullanıcı yöneticisini yapılandırın. UserManager ASP.NET Identity'de tanımlanır ve uygulama tarafından kullanılır.
+    /// <summary>
+    /// Bu uygulamada kullanılan uygulama kullanıcı yöneticisini yapılandırın. UserManager ASP.NET Identity'de tanımlanır ve uygulama tarafından kullanılır.
+    /// </summary>
     public class ApplicationUserManager : UserManager<ApplicationUser>
     {
+        /// <summary>
+        /// User Store
+        /// </summary>
+        /// <param name="store"></param>
         public ApplicationUserManager(IUserStore<ApplicationUser> store)
             : base(store)
         {
         }
 
+        /// <summary>
+        /// Create
+        /// </summary>
+        /// <param name="options"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
         public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context) 
         {
             var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<ApplicationDbContext>()));
@@ -88,19 +116,37 @@ namespace Todo
         }
     }
 
-    // Uygulamada kullanılan uygulama oturum açma yöneticisini yapılandırın.
+    /// <summary>
+    /// Uygulamada kullanılan uygulama oturum açma yöneticisini yapılandırın.
+    /// </summary>
     public class ApplicationSignInManager : SignInManager<ApplicationUser, string>
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="userManager"></param>
+        /// <param name="authenticationManager"></param>
         public ApplicationSignInManager(ApplicationUserManager userManager, IAuthenticationManager authenticationManager)
             : base(userManager, authenticationManager)
         {
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
         public override Task<ClaimsIdentity> CreateUserIdentityAsync(ApplicationUser user)
         {
             return user.GenerateUserIdentityAsync((ApplicationUserManager)UserManager);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="options"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
         public static ApplicationSignInManager Create(IdentityFactoryOptions<ApplicationSignInManager> options, IOwinContext context)
         {
             return new ApplicationSignInManager(context.GetUserManager<ApplicationUserManager>(), context.Authentication);
